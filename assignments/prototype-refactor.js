3336c9d651dd0ac3904777eadc6d5199937427ae
MIAlien <aliass.jr1@gmail.com>
2019-10-10 23:57:28 -0400

    first initial

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,18 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+/* Destroy prototype method for character object  
+dimension is the size, height and width of object character
+*/
+class GameObject {
+    constructor (attributes) {
+    this.createdAt = attributes.createdAt;
+    this.name = attributes.name;
+    this.dimensions = attributes.dimensions;
+    }
+
+    destroy() {
+    return `${this.name} was removed from the game.`;
+    }
+}
