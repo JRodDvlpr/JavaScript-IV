e09cf93555cd6df53dadfcdfc5730c2377da4669
MIAlien <aliass.jr1@gmail.com>
2019-10-11 17:08:58 -0400

    Classes Started

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,55 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age -attributes.age;
+        this.location = attributes.location;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}, and I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.speciality = attributes.speciality;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubject = attributes.favSubject;
+    }
+    listsSubjects(arraySubject) {
+        return arraySubject.map(subject => subject)
+    }
+    PRAssignment(student, subject) {
+        return `${student.name} has submitted a PR for ${subject}`
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(attributes) {
+        super(attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+    standup(name, channel){
+        return `${name} announces to ${channel}, @channel standy times!`
+    }
+    debugsCode(name, student, subject) {
+        return `${name}debugs ${student.name}'s code on ${subject}`
+    }
+}
